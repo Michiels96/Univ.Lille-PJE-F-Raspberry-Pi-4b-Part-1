42ae7318234abcbb711947cc1c1431c1b190843d
Pierre Michiels <pierre.michiels.etu@univ-lille.fr>
2020-09-29 10:44:10 +0200

    update

--- runit/runit.sh
+++ runit/runit.sh
@@ -1,2 +1,3 @@
 #!/bin/bash/
-echo "$(date) - alive" >> ~/scripts/git_scripts/pje-f-raspberry-pi-4b/logs/workload.log
+sleep $((RANDOM % 10));echo '123'
+#&& echo "$(date) - alive" >> ~/scripts/git_scripts/pje-f-raspberry-pi-4b/logs/workload.log
